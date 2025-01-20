46c80a2ea83325dc1f8f781a0b90f67e48e7ff6f
Alexander Densley <densley.alexander@gmail.com>
2025-01-20 09:22:26 -0800

    repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Viewer from '@/components/viewer';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import Viewer from '@/components/viewer'
 
 export default function Home() {
-  return (
-    <main>
-      <Viewer/>
-    </main>
-  );
+	return (
+		<main>
+			<Card>
+				<CardHeader>
+					<CardTitle>PDF Viewer</CardTitle>
+				</CardHeader>
+				<CardContent>hey</CardContent>
+			</Card>
+
+			<Viewer />
+		</main>
+	)
 }
